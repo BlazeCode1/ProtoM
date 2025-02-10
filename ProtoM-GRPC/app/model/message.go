d56d0e4a7d6280eb4a4dcdf23ec59e6539c64f46
Faisal <f9ool_99@hotmail.com>
2025-02-10 18:40:10 +0300

    refactoring proto

---
+++ ProtoM-GRPC/app/model/message.go
@@ -0,0 +1,10 @@
+package model
+
+// ChatMessage represents a message structure
+type ChatMessage struct {
+	ID         string `json:"id"`
+	SenderID   string `json:"sender_id"`
+	ReceiverID string `json:"receiver_id"`
+	Content    string `json:"content"`
+	Timestamp  int64  `json:"timestamp"`
+}
---
+++ ProtoM-GRPC/app/services/message-service.go
@@ -0,0 +1,46 @@
+package service
+
+import (
+	"fmt"
+	"sync"
+	"time"
+
+	"github.com/BlazeCode1/ProtoM-GRPC/app/model"
+	pb "github.com/BlazeCode1/ProtoM-GRPC/proto/gen/chat"
+)
+
+// MessageService handles message operations
+type MessageService struct {
+	messages []model.ChatMessage
+	mu       sync.Mutex
+}
+
+// NewMessageService creates a new instance of MessageService
+func NewMessageService() *MessageService {
+	return &MessageService{}
+}
+
+// SendMessage processes an incoming message
+func (s *MessageService) SendMessage(msg *model.ChatMessage) string {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	msg.ID = fmt.Sprintf("%d", len(s.messages)+1)
+	msg.Timestamp = time.Now().Unix()
+
+	s.messages = append(s.messages, *msg)
+	fmt.Printf("📩 New message from %s: %s\n", msg.SenderID, msg.Content)
+
+	return msg.ID
+}
+
+// StreamMessages streams messages for a user
+func (s *MessageService) StreamMessages(userID string) []*model.ChatMessage {
+	var userMessages []*model.ChatMessage
+	for _, msg := range s.messages {
+		if msg.ReceiverID == userID {
+			userMessages = append(userMessages, &msg)
+		}
+	}
+	return userMessages
+}
